6af34ac9c5be59f9485342d8cf0b158856e515c3
RahulSainy <kradekidrahul@gmail.com>
2020-12-11 17:04:57 +0530

    completed 11 Dec, 2020

--- Traning-App/src/environments/environment.prod.ts
+++ Traning-App/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:  {
+    apiKey: "AIzaSyCz9sv_veZokUYLlqJomPGaMH1CXdAMGic",
+    authDomain: "ng-fitness-tracker-cb4be.firebaseapp.com",
+    projectId: "ng-fitness-tracker-cb4be",
+    storageBucket: "ng-fitness-tracker-cb4be.appspot.com",
+    messagingSenderId: "1025390054012",
+    appId: "1:1025390054012:web:860af35966ffff11ced7f9",
+    measurementId: "G-97ZZH6MQN3"
+  }
 };
